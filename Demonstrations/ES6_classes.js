c57956b6e4d5d770f03eebd6becaaad7cd669b57
I do <ido_p_a@hotmail.com>
2022-08-23 21:52:11 +0300

    added demo , practice solutions

--- Demonstrations/ES6_classes.js
+++ Demonstrations/ES6_classes.js
@@ -13,5 +13,33 @@ class B extends A {
   }
 }
 //
-console.log(a1.getVal());
-console.log(b1.getZal());
+class Person {
+  //fields
+  age = 120;
+  //methods
+  constructor(name) {
+    this.name = name;
+    this.title = 'student';
+    if(name == 'ido') {
+      this.title = 'teacher';
+    }
+    let day  = new Date().getDay();//local variable
+    this.mood = day > 0 ? "working/learning" : "vacation!!";
+  }
+  //
+  printDetails() { console.log(`${this.name},${this.title},${this.mood},${this}`)};
+  //
+  greet(){
+    let word = 'hi , ';//local variable
+    console.log(`${word} ${this.printDetails()}`);
+  }
+}
+//
+let p1 = new Person();
+let p2 = new Person('matanel');
+let p3 = new Person('ido');
+p1.greet();
+p2.printDetails();
+p3.printDetails();
+// console.log(a1.getVal());
+// console.log(b1.getZal());
